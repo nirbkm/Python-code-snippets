781da2d4e977d6e16106ba988aa36ddb648dc7b2
nirbkm <nir@beckermus.com>
2022-10-23 17:02:09 +0300

    added collections_module

---
+++ collections_module.py
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+#################################
+## Collections module examples ##
+#################################
+
+# deque	        A sequence-like collection that supports efficient addition and removal of items from either end of the sequence
+# defaultdict	A dictionary subclass for constructing default values for missing keys and automatically adding them to the dictionary
+# namedtuple()	A factory function for creating subclasses of tuple that provides named fields that allow accessing items by name while keeping the ability to access items by index
+# OrderedDict	A dictionary subclass that keeps the key-value pairs ordered according to when the keys are inserted
+# Counter	    A dictionary subclass that supports convenient counting of unique items in a sequence or iterable
+# ChainMap	    A dictionary-like class that allows treating a number of mappings as a single dictionary object
+
+# Besides these specialized data types, collections also provides three base classes that facilitate the creations of custom lists, dictionaries, and strings:
+# 
+# UserDict	    A wrapper class around a dictionary object that facilitates subclassing dict
+# UserList	    A wrapper class around a list object that facilitates subclassing list
+# UserString	A wrapper class around a string object that facilitates subclassing string
